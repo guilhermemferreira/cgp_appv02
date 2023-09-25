8782631351798ffecb0317c4a10540949e5ba09e
GUILHERME FERREIRA <guilherme.ferreira.30483@esgc.pt>
2023-09-25 20:43:53 +0100

    Clientes

--- cgp_appv02/Program.cs
+++ cgp_appv02/Program.cs
@@ -4,81 +4,117 @@
     {
         class Cliente
         {
-            struct cliente
+            public string Nome;
+            public string Morada;
+            public int NIF;
+            public int NIPC;
+            public DateTime DataNascimento;
+            public int Telefone;
+            public string Nacionalidade;
+            public decimal SaldoTotal;
+
+            public List<Conta> Contas { get; set; }
+            public List<Cartao> Cartoes { get; set; }
+
+            public Cliente(string Nome, string Morada, int NIF, int NIPC, DateTime DataNascimento, int Telefone, string Nacionalidade, decimal SaldoTotal)
             {
-                public string Nome;
-                public string Morada;
-                public int NIF;
-                public int NIPC;
-                public DateTime DataNascimento;
-                public int Telefone;
-                public string Nacionalidade;
-                public decimal SaldoTotal;
+                this.Nome = Nome;
+                this.Morada = Morada;
+                this.NIF = NIF;
+                this.NIPC = NIPC;
+                this.DataNascimento = DataNascimento;
+                this.Telefone = Telefone;
+                this.Nacionalidade = Nacionalidade;
+                this.SaldoTotal = SaldoTotal;
+
+                this.Contas = new List<Conta>();
+                this.Cartoes = new List<Cartao>();
             }
 
-            static cliente CriarCliente(string Nome, string Morada, int NIF, int NIPC, DateTime DataNascimento, int Telefone, string Nacionalidade, int SaldoTotal)
+            public static Cliente CriarCliente()
             {
-                cliente cliente;
+                Console.WriteLine("Insira o nome do cliente:");
+                string Nome = Console.ReadLine();
+                Console.WriteLine("Insira a morada do cliente:");
+                string Morada = Console.ReadLine();
+                Console.WriteLine("Insira o NIF do cliente:");
+                int NIF = int.Parse(Console.ReadLine());
+                Console.WriteLine("Insira o NIPC do cliente:");
+                int NIPC = int.Parse(Console.ReadLine());
+                Console.WriteLine("Insira a data de nascimento do cliente:");
+                DateTime DataNascimento = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Insira o telefone do cliente:");
+                int Telefone = int.Parse(Console.ReadLine());
+                Console.WriteLine("Insira a nacionalidade do cliente:");
+                string Nacionalidade = Console.ReadLine();
+                Console.WriteLine("Insira o saldo total do cliente:");
+                decimal SaldoTotal = decimal.Parse(Console.ReadLine());
 
-                cliente.Nome = Nome;
-                cliente.Morada = Morada;
-                cliente.NIF = NIF;
-                cliente.NIPC = NIPC;
-                cliente.DataNascimento = DataNascimento;
-                cliente.Telefone = Telefone;
-                cliente.Nacionalidade = Nacionalidade;
-                cliente.SaldoTotal = SaldoTotal;
+                Cliente cliente = new Cliente(Nome, Morada, NIF, NIPC, DataNascimento, Telefone, Nacionalidade, SaldoTotal);
 
                 return cliente;
             }
 
-            static cliente LerCliente()
+            public void AdicionarConta(Conta conta)
             {
-                cliente cliente;
-
-                Console.WriteLine("Digite o Nome do Cliente: ");
-                cliente.Nome = Console.ReadLine();
-                Console.WriteLine("Digite a Morada do Cliente: ");
-                cliente.Morada = Console.ReadLine();
-                Console.WriteLine("Digite o NIF do Cliente: ");
-                cliente.NIF = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Digite o NIPC do Cliente: ");
-                cliente.NIPC = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Digite a Data de Nascimento do Cliente: ");
-                cliente.DataNascimento = DateTime.Parse(Console.ReadLine());
-                Console.WriteLine("Digite o Número de Telefone do Cliente: ");
-                cliente.Telefone = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Digite a Nacionalidade do Cliente: ");
-                cliente.Nacionalidade = Console.ReadLine();
-                Console.WriteLine("Digite o Saldo Total do Cliente: ");
-                cliente.SaldoTotal = Decimal.Parse(Console.ReadLine());
-
-                return cliente;
+                this.Contas.Add(conta);
             }
 
-            struct conta
+            public void AdicionarCartao(Cartao cartao)
             {
-                public int N_Conta;
-                public int IBAN;
-                public DateTime DataAbertura;
-                public string TipoConta;
-                public int SaldoConta;
+                this.Cartoes.Add(cartao);
+            }
+
+            public List<Conta> ObterContas()
+            {
+                return this.Contas;
+            }
+
+            public List<Cartao> ObterCartoes()
+            {
+                return this.Cartoes;
+            }
+        }
+
+        class Conta
+        {
+            public int N_Conta;
+            public int IBAN;
+            public DateTime DataAbertura;
+            public string TipoConta;
+            public int SaldoConta;
+
+            public Conta(int N_Conta, int IBAN, DateTime DataAbertura, string TipoConta, int SaldoConta)
+            {
+                this.N_Conta = N_Conta;
+                this.IBAN = IBAN;
+                this.DataAbertura = DataAbertura;
+                this.TipoConta = TipoConta;
+                this.SaldoConta = SaldoConta;
             }
         }
 
         class Cartao
         {
-            struct cartao
+            public int N_Cartao;
+            public DateTime DataValidade;
+            public int SaldoCartao;
+            public int Codigo;
+
+            public Cartao(int N_Cartao, DateTime DataValidade, int SaldoCartao, int Codigo)
             {
-                public int N_Cartao;
-                public DateTime DataValidade;
-                public int SaldoCartao;
-                public int Codigo;
+                this.N_Cartao = N_Cartao;
+                this.DataValidade = DataValidade;
+                this.SaldoCartao = SaldoCartao;
+                this.Codigo = Codigo;
             }
         }
+
         static void Main(string[] args)
         {
-            
+            //Cliente cliente = Cliente.CriarCliente();
+
+
         }
     }
 }
