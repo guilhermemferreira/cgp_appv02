a584648120f29140d9bd533f21f02667dc09cdb9
GUILHERME FERREIRA <guilherme.ferreira.30483@esgc.pt>
2023-09-25 20:10:40 +0100

    cliente update

--- cgp_appv02/Program.cs
+++ cgp_appv02/Program.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        class Clientes
+        class Cliente
         {
             struct cliente
             {
@@ -13,7 +13,47 @@
                 public DateTime DataNascimento;
                 public int Telefone;
                 public string Nacionalidade;
-                public int SaldoTotal;
+                public decimal SaldoTotal;
+            }
+
+            static cliente CriarCliente(string Nome, string Morada, int NIF, int NIPC, DateTime DataNascimento, int Telefone, string Nacionalidade, int SaldoTotal)
+            {
+                cliente cliente;
+
+                cliente.Nome = Nome;
+                cliente.Morada = Morada;
+                cliente.NIF = NIF;
+                cliente.NIPC = NIPC;
+                cliente.DataNascimento = DataNascimento;
+                cliente.Telefone = Telefone;
+                cliente.Nacionalidade = Nacionalidade;
+                cliente.SaldoTotal = SaldoTotal;
+
+                return cliente;
+            }
+
+            static cliente LerCliente()
+            {
+                cliente cliente;
+
+                Console.WriteLine("Digite o Nome do Cliente: ");
+                cliente.Nome = Console.ReadLine();
+                Console.WriteLine("Digite a Morada do Cliente: ");
+                cliente.Morada = Console.ReadLine();
+                Console.WriteLine("Digite o NIF do Cliente: ");
+                cliente.NIF = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite o NIPC do Cliente: ");
+                cliente.NIPC = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite a Data de Nascimento do Cliente: ");
+                cliente.DataNascimento = DateTime.Parse(Console.ReadLine());
+                Console.WriteLine("Digite o Número de Telefone do Cliente: ");
+                cliente.Telefone = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Digite a Nacionalidade do Cliente: ");
+                cliente.Nacionalidade = Console.ReadLine();
+                Console.WriteLine("Digite o Saldo Total do Cliente: ");
+                cliente.SaldoTotal = Decimal.Parse(Console.ReadLine());
+
+                return cliente;
             }
 
             struct conta
@@ -26,12 +66,7 @@
             }
         }
 
-        class Morada 
-        {
-
-        }
-
-        class Cartoes
+        class Cartao
         {
             struct cartao
             {
