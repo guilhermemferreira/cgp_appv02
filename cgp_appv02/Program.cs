8c62f3f32777b3c5ec3d335cafed73a189e7f089
guilh <guilh@MSI>
2023-09-25 19:05:58 +0100

    Adicionar arquivos de projeto.

---
+++ cgp_appv02/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace cgp_appv02
+{
+    internal class Program
+    {
+        class Clientes
+        {
+            struct clientes
+            {
+                public string Nome;
+                public string Morada;
+                public DateTime DataNascimento;
+            }
+        }
+
+        class Morada
+        {
+
+        }
+
+        class Cartoes
+        {
+            
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
