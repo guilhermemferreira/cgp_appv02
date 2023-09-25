f69210afb280b8a8a9ad1bfb8c9177ca5eead0d4
GUILHERME FERREIRA <guilherme.ferreira.30483@esgc.pt>
2023-09-25 19:19:20 +0100

    struct ready

--- cgp_appv02/Program.cs
+++ cgp_appv02/Program.cs
@@ -4,22 +4,40 @@
     {
         class Clientes
         {
-            struct clientes
+            struct cliente
             {
                 public string Nome;
                 public string Morada;
+                public int NIF;
+                public int NIPC;
                 public DateTime DataNascimento;
+                public int Telefone;
+                public string Nacionalidade;
+                public int SaldoTotal;
+            }
+
+            struct conta
+            {
+                public int N_Conta;
+                public int IBAN;
+                public DateTime DataAbertura;
+                public string TipoConta;
+                public int SaldoConta;
             }
         }
 
-        class Morada
+        class Morada 
         {
 
         }
 
         class Cartoes
         {
-            
+            struct cartao
+            {
+                public int N_Cartao;
+                public DateTime
+            }
         }
         static void Main(string[] args)
         {
