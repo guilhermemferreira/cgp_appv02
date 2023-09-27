a5a2330f53edde92aeb4df820c9b258d079c3fe9
GUILHERME FERREIRA <guilherme.ferreira.30483@esgc.pt>
2023-09-27 16:13:29 +0100

    Estruturamento Base - Done

--- cgp_appv02/Program.cs
+++ cgp_appv02/Program.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.NetworkInformation;
 
 namespace cgp_appv02
 {
     internal class Program
     {
+        #region Estrutura Clientes
         struct cliente
         {
             public string Nome;
@@ -15,7 +17,6 @@ namespace cgp_appv02
             public int Telefone;
             public string Nacionalidade;
         }
-
         static cliente CriarCliente()
         {
             cliente cliente;
@@ -51,6 +52,164 @@ namespace cgp_appv02
                 EscreverClienteEcra(cliente);
             }
         }
+        #endregion
+
+        #region Conta
+        struct conta
+        {
+            public int NConta;
+            public int IBAN;
+            public DateTime DataAbertura;
+            public string TipoConta;
+            public decimal SaldoTotal;
+        }
+
+        static conta CriarConta(int NConta, int IBAN, DateTime DataAbertura, string TipoConta, decimal SaldoTotal)
+        {
+            conta conta;
+            conta.NConta = NConta; 
+            conta.IBAN = IBAN;
+            conta.DataAbertura = DataAbertura;
+            conta.TipoConta = TipoConta;
+            conta.SaldoTotal = SaldoTotal;
+
+            return conta;
+        }
+
+        static int QuantasContas()
+        {
+            Console.WriteLine("Digite quantas contas pretende criar: ");
+            int quantascontas = Convert.ToInt32(Console.ReadLine());
+
+            return quantascontas;
+        }
+
+        static conta[] CriarArrayConta(int quantascontas) 
+        {
+            conta[] arrayconta = new conta[quantascontas];
+            return arrayconta;
+        }
+
+        static conta LerConta()
+        {
+            conta conta;
+            conta.NConta = 0;
+            conta.IBAN = 0;
+            Console.WriteLine("Digite a data de abertura da Conta: ");
+            conta.DataAbertura = DateTime.Parse(Console.ReadLine());    
+            Console.WriteLine("Digite o Tipo de Conta: ");
+            conta.TipoConta = Console.ReadLine();
+            Console.WriteLine("Digite o Saldo da Conta: ");
+            conta.SaldoTotal = Decimal.Parse(Console.ReadLine());   
+
+            return conta;
+        }
+
+        static conta[] LerContas(conta[] arrayconta, int quantascontas)
+        {
+            conta atemp;
+            for(int i = 0; i < quantascontas; i++)
+            {
+                Console.WriteLine("Conta {0}", i + 1);
+                atemp = LerConta();
+                arrayconta[i] = atemp;
+            }
+            return arrayconta;
+        }
+
+        static void EscreverConta(conta conta)
+        {
+            Console.WriteLine("Conta: {0}, Tipo: {1}, Saldo: {2}",
+                conta.NConta, conta.TipoConta, conta.SaldoTotal);
+        }
+
+        static void EscreverContas(conta[] arrayconta, int quantascontas)
+        {
+            conta atemp;
+            for(int i = 0; i < quantascontas;i++)
+            {
+                atemp = arrayconta[i];
+                EscreverConta(atemp);
+            }
+        }
+        #endregion
+
+        #region Estrutura Cartao
+        struct cartao
+        {
+            public int NCartao;
+            public DateTime DataValidade;
+            public decimal SaldoCartao;
+            public int Codigo;
+        }
+
+        static cartao CriarCartao(int NCartao, DateTime DataValidade, decimal SaldoCartao, int Codigo)
+        {
+            cartao cartao;
+            cartao.NCartao = NCartao;
+            cartao.DataValidade = DataValidade;
+            cartao.SaldoCartao = SaldoCartao;
+            cartao.Codigo = Codigo;
+
+            return cartao;
+        }
+
+        static int QuantosCartoes()
+        {
+            Console.WriteLine("Quantos Cartões é que quer associar à Conta: ");
+            int quantoscartoes = Convert.ToInt32(Console.ReadLine());
+
+            return quantoscartoes;
+        }
+
+        static cartao[] CriarArrayCartao(int quantoscartoes)
+        {
+            cartao[] arraycartao = new cartao[quantoscartoes];
+            return arraycartao;
+        }
+
+        static cartao CriarCartao()
+        {
+            cartao cartao;
+            cartao.NCartao = 0;
+            Console.WriteLine("Data de Validade: ");
+            cartao.DataValidade = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Saldo do Cartão: ");
+            cartao.SaldoCartao = Decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Codigo do Cartão: ");
+            cartao.Codigo = Convert.ToInt32(Console.ReadLine());
+
+            return cartao;
+        }
+
+        static cartao[] LerCartoes(cartao[] arraycartao,int quantoscartoes)
+        {
+            cartao atemp;
+            for(int i = 0; i < quantoscartoes; i++)
+            {
+                Console.WriteLine("Cartão: {0}", i + 1);
+                atemp = CriarCartao();
+                arraycartao[i] = atemp;
+            }
+            return arraycartao;
+        }
+
+        static void EscreverCartao(cartao cartao) 
+        {
+            Console.WriteLine("Nº Conta: {0}, Saldo: {1}, Codigo: {2}",
+                cartao.NCartao, cartao.SaldoCartao, cartao.Codigo);
+        }
+
+        static void EscreverCartoes(cartao[] arraycartao, int quantoscartoes)
+        {
+            cartao atemp;
+            for(int i = 0; i < quantoscartoes;i++)
+            {
+                atemp = arraycartao[i];
+                EscreverCartao(atemp);
+            }
+        }
+        #endregion
 
         static int Menu()
         {
